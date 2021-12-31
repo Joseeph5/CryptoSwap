f33b678ec6062fb3e6037a5a7d5b9041a9c17df3
joseeph5 <youssef.beenamor@gmail.com>
2021-12-31 15:31:35 +0100

    add transferFrom function

--- contracts/JosephToken.sol
+++ contracts/JosephToken.sol
@@ -9,9 +9,11 @@ contract JosephToken{
     // Token total supply
     uint256 public totalSupply;
     // Token holders address
-    mapping(address => uint256) private balances;
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowances;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     constructor(uint256 _initialSupply) {
         totalSupply = _initialSupply;
@@ -30,16 +32,44 @@ contract JosephToken{
         return 18;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool){
-        uint256 senderBalance = balances[msg.sender];
-        require(senderBalance >= _value,"ERC20: transfer amount exceeds balance");
+    function transferTo(address _from, address _to, uint256 _amount) public returns (bool){
+        uint256 senderBalance = balances[_from];
+        require(senderBalance >= _amount,"ERC20: transfer amount exceeds balance");
 
         unchecked {
-            balances[msg.sender] = senderBalance - _value;
+            balances[msg.sender] = senderBalance - _amount;
         }
-        balances[_to] += _value;
+        balances[_to] += _amount;
 
-        emit Transfer(msg.sender,_to,_value);
+        emit Transfer(msg.sender,_to,_amount);
+
+        return true;
+    }
+
+    function transfer(address _to, uint256 _amount) public returns (bool){
+        transferTo(msg.sender, _to, _amount);
+        return true;
+    }
+
+    function approveFrom(address _owner,address _spender, uint256 _amount) public returns (bool) {
+        allowances[_owner][_spender] = _amount;
+        emit Approval(_owner, _spender, _amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        approveFrom(msg.sender, spender, amount);
+        return true;
+    }
+
+
+    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool){
+        uint256 currentAllowance = allowances[_from][msg.sender];
+        require(currentAllowance >= _amount, "ERC20: transfer amount exceeds allowance");
+        
+        approveFrom(_from, msg.sender, currentAllowance - _amount);
+
+        transferTo(_from,_to,_amount);
 
         return true;
     }
--- test/JosephToken.js
+++ test/JosephToken.js
@@ -6,57 +6,93 @@ contract('JosephToken', (accounts) => {
     instance = await JosephToken.deployed();
   });
 
-  it('total supply test passed 🎉', async () => {
-    const totalSupply = await instance.totalSupply();
-    assert.equal(totalSupply.toNumber(), 1000000);
-    const ownerBalance = await instance.balanceOf(accounts[0]);
-    assert.equal(ownerBalance, 1000000);
-  });
+  describe('Token deployemnt', async () => {
+    it('total supply test passed 🎉', async () => {
+      const totalSupply = await instance.totalSupply();
+      assert.equal(totalSupply.toNumber(), 1000000);
+      const ownerBalance = await instance.balanceOf(accounts[0]);
+      assert.equal(ownerBalance, 1000000);
+    });
 
-  it('name and symbol test passed 🎉', async () => {
-    const name = await instance.name();
-    const symbol = await instance.symbol();
-    assert.equal(name, 'JosephToken');
-    assert.equal(symbol, 'JOT');
-  });
+    it('name and symbol test passed 🎉', async () => {
+      const name = await instance.name();
+      const symbol = await instance.symbol();
+      assert.equal(name, 'JosephToken');
+      assert.equal(symbol, 'JOT');
+    });
 
-  it('transaction test passed 🎉', async () => {
-    const success = await instance.transfer.call(accounts[1], 250000);
-    assert.equal(success, true, 'it returns true');
-    const receipt = await instance.transfer(accounts[1], 250000);
-    assert.equal(receipt.logs.length, 1, 'triggers one event');
-    assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
-    assert.equal(
-      receipt.logs[0].args._from,
-      accounts[0],
-      'logs the account the tokens are transferred from'
-    );
-    assert.equal(
-      receipt.logs[0].args._to,
-      accounts[1],
-      'logs the account the tokens are transferred to'
-    );
-    assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
-    const receiverBalance = await instance.balanceOf(accounts[1]);
-    assert.equal(
-      receiverBalance.toNumber(),
-      250000,
-      'adds the amount to the receiving account'
-    );
-    const ownerBalance = await instance.balanceOf(accounts[0]);
-    assert.equal(
-      ownerBalance.toNumber(),
-      750000,
-      'deducts the amount from the sending account'
-    );
-  });
+    it('transaction test passed 🎉', async () => {
+      const success = await instance.transfer.call(accounts[1], 250000);
+      assert.equal(success, true, 'it returns true');
+      const receipt = await instance.transfer(accounts[1], 250000);
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      assert.equal(
+        receipt.logs[0].args._from,
+        accounts[0],
+        'logs the account the tokens are transferred from'
+      );
+      assert.equal(
+        receipt.logs[0].args._to,
+        accounts[1],
+        'logs the account the tokens are transferred to'
+      );
+      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+      const receiverBalance = await instance.balanceOf(accounts[1]);
+      assert.equal(
+        receiverBalance.toNumber(),
+        250000,
+        'adds the amount to the receiving account'
+      );
+      const ownerBalance = await instance.balanceOf(accounts[0]);
+      assert.equal(
+        ownerBalance.toNumber(),
+        750000,
+        'deducts the amount from the sending account'
+      );
+    });
 
-  it(`transferring something larger than the sender's balance`, async () => {
-    try {
-      const success = await instance.transfer.call(accounts[1], 9999999);
-    } catch (error) {
-      assert(error.message.indexOf('revert') >= 0);
-    }
+    it(`can't transfer something larger than the sender's balance 🎉`, async () => {
+      try {
+        const success = await instance.transfer.call(accounts[1], 9999999);
+      } catch (error) {
+        assert(error.message.indexOf('revert') >= 0);
+      }
+    });
+
+    it('approve test passed 🎉', async () => {
+      const success = await instance.approve.call(accounts[1], 250000);
+      assert.equal(success, true, 'it returns true');
+
+      const receipt = await instance.approve(accounts[1], 250000);
+      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+
+      const allowance = await instance.allowances(accounts[0], accounts[1]);
+      assert.equal(allowance.toNumber(), 250000, 'store the allowed amount');
+    });
+
+    it('transferFrom test passed 🎉', async () => {
+      const fromAccount = accounts[2];
+      const toAccount = accounts[3];
+      const spenderAccount = accounts[4];
+
+      const receipt = await instance.transfer(fromAccount, 200000);
+      const approve = await instance.approve(spenderAccount, 200000, {
+        from: fromAccount,
+      });
+      let allowance = await instance.allowances(fromAccount, spenderAccount);
+      assert.equal(allowance.toNumber(), 200000, 'store the allowed amount');
+
+      const success = await instance.transferFrom.call(fromAccount, toAccount, 100000, {
+        from: spenderAccount,
+      });
+      assert.equal(success, true, 'it returns false');
+      const transferFrom = await instance.transferFrom(fromAccount, toAccount, 100000, {
+        from: spenderAccount,
+      });
+      allowance = await instance.allowances(fromAccount, spenderAccount);
+      assert.equal(allowance.toNumber(), 100000, 'store the allowed amount');
+    });
   });
 });
 
